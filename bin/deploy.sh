fd2b65ff4fc3a7c8a4b7a5fbf860652b38e841b2
Hidenori FUJIMURA <hfu@si.local>
2015-11-15 09:36:58 +0900

    Update

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -28,7 +28,7 @@ tests" > .gitignore
 
 git init
 git config user.name "Travis CI"
-git config user.email "miya+github.com@wpist.me"
+# git config user.email "miya+github.com@wpist.me"
 git add .
 git commit --quiet -m "Deploy from travis"
 git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
